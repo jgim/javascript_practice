7ffe265a1865257b81343c81c1a01ceaf724e4bd
Jongseok Gim <jgim@c7r4s3.42seoul.kr>
2022-07-06 22:48:00 +0900

    ~function

---
+++ immutable.js
@@ -0,0 +1,24 @@
+const obj = { a: 1 };
+const copy = Object.assign({}, obj);
+console.log(copy); // { a: 1 }
+console.log(obj == copy); // false
+
+// Merge
+const o1 = { a: 1 };
+const o2 = { b: 2 };
+const o3 = { c: 3 };
+
+const merge1 = Object.assign(o1, o2, o3);
+
+console.log(merge1); // { a: 1, b: 2, c: 3 }
+console.log(o1);     // { a: 1, b: 2, c: 3 }, 타겟 객체가 변경된다!
+
+// Merge
+const o4 = { a: 1 };
+const o5 = { b: 2 };
+const o6 = { c: 3 };
+
+const merge2 = Object.assign({}, o4, o5, o6);
+
+console.log(merge2); // { a: 1, b: 2, c: 3 }
+console.log(o4);     // { a: 1 }
---
+++ property.js
@@ -0,0 +1,20 @@
+var person = {
+  'first-name': 'Ung-mo',
+  'last-name': 'Lee',
+  gender: 'male',
+  1: 10
+};
+
+console.log(person);
+
+// console.log(person.first-name);    // NaN: undefined-undefined
+// console.log(person[first-name]);   // ReferenceError: first is not defined
+console.log(person['first-name']); // 'Ung-mo'
+
+console.log(person.gender);    // 'male'
+// console.log(person[gender]);   // ReferenceError: gender is not defined
+console.log(person['gender']); // 'male'
+
+console.log(person['1']); // 10
+console.log(person[1]);   // 10 : person[1] -> person['1']
+// console.log(person.1);    // SyntaxError
